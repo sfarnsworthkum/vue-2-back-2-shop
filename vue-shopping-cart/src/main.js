f841458ddf09454c598700599d90aa3422d27e41
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-11-01 13:06:19 -0700

    adding currency filter to main.js

--- vue-shopping-cart/src/main.js
+++ vue-shopping-cart/src/main.js
@@ -1,7 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue';
+import App from './App.vue';
+import Element from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import 'element-ui/lib/theme-default/index.css';
+import NavBar from './components/NavBar.vue';
+import ProductList from './components/ProductList.vue';
+import ShoppingCart from './components/ShoppingCart.vue';
+// Services
+Vue.use(Element, { locale });
+// Components
+Vue.component('NavBar', NavBar);
+Vue.component('ProductList', ProductList);
+Vue.component('ShoppingCart', ShoppingCart);
+// Filters
+Vue.filter('currency', function(value) {
+  return '$' + parseFloat(value / 100).toFixed(2);
+});
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
